6fd145a7c34ed37911148c8378b8fbd1bb25ee35
Randy Stauner <randy@magnificent-tears.com>
2011-11-14 17:36:20 -0700

    initial commit

---
+++ lib/HTML/FromANSI/Tiny.pm
@@ -0,0 +1,14 @@
+# vim: set ts=2 sts=2 sw=2 expandtab smarttab:
+use strict;
+use warnings;
+
+package HTML::FromANSI::Tiny;
+# ABSTRACT: undef
+
+1;
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut

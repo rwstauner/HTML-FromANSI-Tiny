9dbb07d350e960165f615dc9b88841732dea8869
Randy Stauner <randy@magnificent-tears.com>
2011-11-14 19:30:11 -0700

    Commit initial working verison

--- lib/HTML/FromANSI/Tiny.pm
+++ lib/HTML/FromANSI/Tiny.pm
@@ -3,7 +3,49 @@ use strict;
 use warnings;
 
 package HTML::FromANSI::Tiny;
-# ABSTRACT: undef
+# ABSTRACT: Easily convert command line output to html
+
+sub new {
+  my $class = shift;
+  my $self = {
+    @_ == 1 ? %{ $_[0] } : @_,
+  };
+
+  require HTML::Entities
+    if !$self->{html_encoder};
+
+  bless $self, $class;
+}
+
+sub html {
+  my ($self, $text) = @_;
+  $text = $self->ansi_parser->parse($text)
+    unless ref($text) eq 'ARRAY';
+
+  my @html = map {
+    '<span class="' .  join(' ', @{ $_->[0] }) . '">' .
+      $self->html_encode($_->[1]) . '</span>'
+  } @$text;
+
+  return defined($self->{join}) || !wantarray
+    ? join($self->{join}||'', @html)
+    : @html;
+}
+
+sub ansi_parser {
+  my ($self) = @_;
+  $self->{ansi_parser} ||= do {
+    require Parse::ANSIColor::Tiny;
+    Parse::ANSIColor::Tiny->new();
+  };
+}
+
+sub html_encode {
+  my ($self, $text) = @_;
+  return $self->{html_encoder}->($text)
+    if $self->{html_encoder};
+  return HTML::Entities::encode_entities($text);
+}
 
 1;
 

68cbfe2d588945ee17d7abdb8602b17bd59b03ba
Randy Stauner <randy@magnificent-tears.com>
2011-11-14 19:43:10 -0700

    Add pod

--- lib/HTML/FromANSI/Tiny.pm
+++ lib/HTML/FromANSI/Tiny.pm
@@ -51,6 +51,46 @@ sub html_encode {
 
 =head1 SYNOPSIS
 
+  use HTML::FromANSI::Tiny;
+  my $h = HTML::FromANSI::Tiny->new();
+
+  # output from some command
+  my $output = "\e[31mfoo\033[1;32mbar\033[0m";
+
+  print $h->html($output);
+  # prints '<span class="red">foo</span><span class="bold green">bar</span>'
+
 =head1 DESCRIPTION
+
+Convert the output from a terminal command that is decorated
+with ANSI escape sequences into customizable HTML
+(with a small amount of code).
+
+This module complements L<Parse::ANSIColor::Tiny>
+by providing a simple HTML markup around its output.
+
+L<Parse::ANSIColor::Tiny> returns a data structure that's easy
+to reformat into any desired output.
+Reformatting to HTML seemed simple and common enough
+to warrant this module as well.
+
+=head1 COMPARISON TO HTML::FromANSI
+
+L<HTML::FromANSI> is a bit antiquated (as of v2.03 released in 2007).
+It uses C<font> tags and the C<style> attribute
+and isn't very customizable.
+
+It uses L<Term::VT102> which is probably more robust than
+L<Parse::ANSIColor::Tiny> but may be overkill for simple situations.
+I've also had trouble installing it in the past.
+
+For many simple situations this module combined with L<Parse::ANSIColor::Tiny>
+is likely sufficient and is considerably smaller.
+
+=head1 SEE ALSO
+
+=for :list
+* L<Parse::ANSIColor::Tiny>
+* L<HTML::FromANSI>
 
 =cut
